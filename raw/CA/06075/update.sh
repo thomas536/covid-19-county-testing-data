0cd63a5287132c27645156aaeb7d809272fa0684
Thomas Garner <thomas536@gmail.com>
2020-06-01 23:32:47 -0700

    Update all data through today (now)

---
+++ raw/CA/06075/update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget --no-proxy -O "rows.csv?accessType=DOWNLOAD" "https://data.sfgov.org/api/views/nfpa-mg4g/rows.csv?accessType=DOWNLOAD"
---
+++ raw/TX/update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget --no-proxy --no-check-certificate -O "TexasCOVID-19CumulativeTestsOverTimebyCounty.xlsx" "https://dshs.texas.gov/coronavirus/TexasCOVID-19CumulativeTestsOverTimebyCounty.xlsx"
